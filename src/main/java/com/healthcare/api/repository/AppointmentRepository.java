eebc5f80f115482481582763e4d9047e36f20ada
nguyensan3108 <nguyensan3108@gmail.com>
2026-04-27 00:27:39 +0700

    feat: implement pagination for doctor/patient to get appointments

--- src/main/java/com/healthcare/api/repository/AppointmentRepository.java
+++ src/main/java/com/healthcare/api/repository/AppointmentRepository.java
@@ -1,9 +1,15 @@
 package com.healthcare.api.repository;
 
+import com.healthcare.api.dto.response.AppointmentResponse;
 import com.healthcare.api.entity.Appointment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
+    Page<Appointment> findByDoctorId(UUID doctorId, Pageable pageable);
+
+    Page<Appointment> findByPatientId(UUID patientId, Pageable pageable);
 }
