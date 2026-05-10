b2876d0ba5493b4ae3702ff1a9bf78a8afd257a6
nguyensan3108 <nguyensan3108@gmail.com>
2026-05-11 00:29:53 +0700

    test: complete coverage for get my appointments API in appointment service

--- src/test/java/com/healthcare/api/service/impl/AppointmentServiceImplTest.java
+++ src/test/java/com/healthcare/api/service/impl/AppointmentServiceImplTest.java
@@ -7,10 +7,7 @@ import com.healthcare.api.event.AppointmentBookedEvent;
 import com.healthcare.api.exception.AppException;
 import com.healthcare.api.exception.ErrorCode;
 import com.healthcare.api.mapper.AppointmentMapper;
-import com.healthcare.api.repository.AppointmentRepository;
-import com.healthcare.api.repository.PatientRepository;
-import com.healthcare.api.repository.ScheduleRepository;
-import com.healthcare.api.repository.UserRepository;
+import com.healthcare.api.repository.*;
 import com.healthcare.api.utils.security.SecurityUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,8 +16,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -43,7 +45,8 @@ class AppointmentServiceImplTest {
     private AppointmentMapper appointmentMapper;
     @Mock
     private ApplicationEventPublisher eventPublisher;
-
+    @Mock
+    private DoctorRepository doctorRepository;
     @InjectMocks
     private AppointmentServiceImpl appointmentService;
 
@@ -256,6 +259,92 @@ class AppointmentServiceImplTest {
             assertEquals(ErrorCode.PATIENT_NOT_FOUND, exception.getErrorCode());
             verify(appointmentRepository, never()).save(any());
             verify(scheduleRepository, never()).save(any());
+        }
+    }
+
+    @Test
+    void getMyAppointments_Doctor_Success(){
+        Role role = new Role();
+        role.setName("DOCTOR");
+
+        User user = new User();
+        user.setId(UUID.randomUUID());
+        user.setEmail("doctor@gmail.com");
+        user.setRole(role);
+
+        Doctor doctor = new Doctor();
+        doctor.setId(user.getId());
+        doctor.setUser(user);
+
+        int page = 1;
+        int size = 10;
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        List<Appointment> listAppointments = new ArrayList<>();
+        listAppointments.add(new Appointment());
+        PageImpl<Appointment> appointmentPage = new PageImpl<>(listAppointments, pageable, 1);
+
+        AppointmentResponse expectedResponse = new AppointmentResponse();
+
+        when(userRepository.findByEmail(any())).thenReturn(Optional.of(user));
+        when(doctorRepository.findByUserId(any())).thenReturn(Optional.of(doctor));
+        when(appointmentRepository.findByDoctorId(eq(doctor.getId()),any())).thenReturn(appointmentPage);
+        when(appointmentMapper.toAppointmentResponse(any())).thenReturn(expectedResponse);
+
+        try(var mockedSecurityUtils = mockStatic(SecurityUtils.class)) {
+            mockedSecurityUtils.when(SecurityUtils::getCurrentUserEmail).thenReturn(user.getEmail());
+            var result = appointmentService.getMyAppointments(page, size);
+
+            assertNotNull(result);
+            assertEquals(1, result.getTotalElements());
+            assertEquals(1, result.getData().size());
+            assertEquals(expectedResponse, result.getData().get(0));
+
+            verify(userRepository, times(1)).findByEmail(any());
+            verify(appointmentRepository, times(1)).findByDoctorId(eq(doctor.getId()),any());
+        }
+    }
+
+    @Test
+    void getMyAppointments_Patient_Success(){
+        Role role = new Role();
+        role.setName("PATIENT");
+
+        User user = new User();
+        user.setId(UUID.randomUUID());
+        user.setEmail("patient@gmail.com");
+        user.setRole(role);
+
+        Patient patient = new Patient();
+        patient.setId(user.getId());
+        patient.setUser(user);
+
+        int page = 1;
+        int size = 10;
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        List<Appointment> listAppointments = new ArrayList<>();
+        listAppointments.add(new Appointment());
+        PageImpl<Appointment> appointmentPage = new PageImpl<>(listAppointments,  pageable, 1);
+
+        AppointmentResponse expectedResponse = new AppointmentResponse();
+
+        when(userRepository.findByEmail(any())).thenReturn(Optional.of(user));
+        when(patientRepository.findByUserId(any())).thenReturn(Optional.of(patient));
+        when(appointmentRepository.findByPatientId(eq(patient.getId()),any())).thenReturn(appointmentPage);
+        when(appointmentMapper.toAppointmentResponse(any())).thenReturn(expectedResponse);
+
+        try(var mockedSecurityUtils = mockStatic(SecurityUtils.class)) {
+            mockedSecurityUtils.when(SecurityUtils::getCurrentUserEmail).thenReturn(user.getEmail());
+            var result = appointmentService.getMyAppointments(page, size);
+
+            assertNotNull(result);
+            assertEquals(1, result.getTotalElements());
+            assertEquals(1, result.getData().size());
+            assertEquals(expectedResponse, result.getData().get(0));
+
+            verify(userRepository, times(1)).findByEmail(any());
+            verify(appointmentRepository, times(1)).findByPatientId(eq(patient.getId()),any());
         }
     }
 }
